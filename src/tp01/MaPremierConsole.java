855fa1ba58c456f8f2cb78bafe31b88948fa8662
zhang <zhang@10.188.43.16>
2022-10-13 21:28:25 +0200

    corrige

--- src/tp01/MaPremierConsole.java
+++ src/tp01/MaPremierConsole.java
@@ -1,11 +1,11 @@
 package tp01;
 
-//import util.Console;
+import util.Console;
 
 public class MaPremierConsole {
 public static void main(String[]args) {
-//	Console c = new Console();
-//	c.println("Claude");
-//	c.println("Montacié");
+	Console c = new Console();
+	c.println("Claude");
+	c.println("Montacié");
 	}
 }
